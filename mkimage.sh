f737c80b5f71eccf6d2db46bfaa98430f222445a
Pierre <pierre_langlois@etu.u-bourgogne.fr>
2012-12-02 02:43:27 +0100

    brand new mkimage.sh script. It builds a working image ready to be written on the SD card!

--- mkimage.sh
+++ mkimage.sh
@@ -1,13 +1,55 @@
 #!/bin/sh
 
-ERROR="ERROR:"
+######################################################################################################################################################
+# function declaration
+######################################################################################################################################################
 
 usage()
 {
-  echo "/-----------------------------------------\\"
-  echo "| usage: ./mkimage.sh <output image file> |"
-  echo "\\-----------------------------------------/"
+  echo "/----------------------------------------------\\"
+  echo "| usage: ./mkimage.sh <output image file> [-v] |"
+  echo "\\----------------------------------------------/"
 }
+
+message()
+{
+  echo -e "\033[1m\033[32m====> \033[34m$1\033[0m"
+}
+
+getdirsize()
+{
+  echo `du $1 -s -B $BLK_SIZE | grep -o "[0-9]\+"`
+}
+
+######################################################################################################################################################
+# initial checkings
+######################################################################################################################################################
+
+if [ $UID != 0 ]
+then
+  echo "you need to be root to run this script."
+  usage
+  exit
+fi
+
+if [ $# != 1 ]
+then
+  if [ $# = 2 ]
+  then
+    if [ $2 = "-v" ]
+    then
+      export VERBOSE="yes"
+    else
+      usage
+      exit
+    fi
+  else
+    usage
+    exit
+  fi
+else
+  export VERBOSE="no"
+fi
 
 if [ $0 != "./mkimage.sh" ]
 then
@@ -16,53 +58,176 @@ then
   exit
 fi
 
-if [ $# != 1 ]
+export NEEDED_PROGRAMS="bc mkfs.vfat mkfs.ext2";
+
+for PROG in $NEEDED_PROGRAMS
+do
+  export PROG=$PROG
+  if [ ! `which $PROG 2> /dev/null; echo #?` ]
+  then
+    echo "you need $PROG installed in your system and present in PATH."
+    usage
+    exit
+  fi
+done
+
+######################################################################################################################################################
+# variables initialization
+######################################################################################################################################################
+
+if [ $VERBOSE = "yes" ]
 then
-  echo "$ERROR: you need to provide an output image file name."
-  usage
-  exit
+  export REDIRECT=`tty`
+else
+  export REDIRECT="/dev/null"
 fi
 
-if [ -f $1 ]
+export RFS_DIR="rootfs"
+export MNT_DIR="mnt"
+export FIRWARE_DIR="firmware/boot"
+export CONFIG_DIR="config/linux/boot"
+
+export BLK_SIZE=512
+
+export IMG=$1
+export LOOP
+export OFFSET=4096
+
+
+export RFS_SIZE=`getdirsize $RFS_DIR`
+export FIRWARE_SIZE=`getdirsize firmware/boot`
+export CONFIG_SIZE=`getdirsize config/linux/boot`
+export BOOT_SIZE=`echo "$CONFIG_SIZE + $FIRWARE_SIZE + $OFFSET" | bc`
+
+export FULL_SIZE=`echo "$RFS_SIZE + $BOOT_SIZE + $OFFSET" | bc`
+
+export HEADS=255
+export SECTORS=63
+export CYLINDERS=`echo "$FULL_SIZE/($HEADS*$SECTORS)" | bc`
+
+######################################################################################################################################################
+# creating and empty image and associating it to a loop device
+######################################################################################################################################################
+
+message "creating an emty `echo "($BLK_SIZE * $FULL_SIZE)/1000000" | bc` megabytes image"
+
+dd if=/dev/zero of=$IMG bs=$BLK_SIZE count=$FULL_SIZE > $REDIRECT 2>&1
+LOOP_IMG=`losetup -f --show $IMG`
+LOOP+=" $LOOP_IMG"
+
+message "image associated to '$LOOP_IMG'"
+
+######################################################################################################################################################
+# setting up disk
+######################################################################################################################################################
+
+message "creating partition table with $CYLINDERS cylinders, $HEADS heads and $SECTORS sectors"
+echo w | fdisk -H $HEADS -C $CYLINDERS -S $SECTORS $LOOP_IMG > $REDIRECT 2>&1
+
+message "probing fdisk for the first sector available"
+export PART_BOOT_START=`
+  {
+    echo n
+    echo p
+    echo 1 
+  } | fdisk -H $HEADS -C $CYLINDERS -S $SECTORS $LOOP_IMG | grep -o "default [0-9][0-9]\+" | grep -o "[0-9]\+"
+`
+
+export PART_BOOT_START_B=`echo "$BLK_SIZE * $PART_BOOT_START" | bc`
+export PART_BOOT_END=`echo "$BOOT_SIZE + $PART_BOOT_START" | bc`
+
+message "creating first partition from sector $PART_BOOT_START to sector $PART_BOOT_END, which will later contain the bootloader and the linux kernel"
+{
+  echo n
+  echo p
+  echo 1
+  echo $PART_BOOT_START
+  echo $PART_BOOT_END
+  echo w
+} | fdisk -H $HEADS -C $CYLINDERS -S $SECTORS $LOOP_IMG > $REDIRECT 2>&1
+
+message "setting up flags for the first partition"
+{
+  echo t
+  echo 1
+  echo c
+  echo w
+} | fdisk -H $HEADS -C $CYLINDERS -S $SECTORS $LOOP_IMG > $REDIRECT 2>&1
+
+message "probing fdisk for the next and the last sectors available"
+{
+  echo n
+  echo p
+  echo 2
+  echo
+} | fdisk -H $HEADS -C $CYLINDERS -S $SECTORS $LOOP_IMG | grep -o "default [0-9][0-9]\+" | grep -o "[0-9]\+" > default.tmp 2> $REDIRECT
+
+read PART_RFS_START < default.tmp
+cat default.tmp | tail -n 1 > default_rfs.tmp
+read PART_RFS_END < default_rfs.tmp
+rm default.tmp default_rfs.tmp
+
+export PART_RFS_START=echo $PART_RFS_START_END | head -n 1
+export PART_RFS_START_B=`echo "$BLK_SIZE * $PART_RFS_START" | bc`
+export PART_RFS_END=$PART_RFS_END
+
+message "creating second partition from sector $PART_RFS_START to sector $PART_RFS_END, which will contain the root file system"
+{
+  echo n
+  echo p
+  echo 2
+  echo $PART_RFS_START
+  echo $PART_RFS_END
+  echo w
+} | fdisk -H $HEADS -C $CYLINDERS -S $SECTORS $LOOP_IMG > $REDIRECT 2>&1
+
+######################################################################################################################################################
+# setting up file systems and installing the files on the partitions
+######################################################################################################################################################
+
+
+
+LOOP_IMG_BOOT=`losetup -f --show -o $PART_BOOT_START_B $IMG`
+LOOP+=" $LOOP_IMG_BOOT"
+
+LOOP_IMG_RFS=`losetup -f --show -o $PART_RFS_START_B $IMG`
+LOOP+=" $LOOP_IMG_RFS"
+
+message "associated loop devices '$LOOP_IMG_BOOT' and '$LOOP_IMG_RFS' to each partition"
+
+message "setting up file systems in FAT16 and ext2"
+
+mkfs.vfat -F 16 $LOOP_IMG_BOOT > $REDIRECT 2>&1
+
+mkfs.ext2 $LOOP_IMG_RFS > $REDIRECT 2>&1
+
+if [ ! `mountpoint $MNT_DIR -q; echo $?` ]
 then
-  echo "$ERROR the file $1 already exists."
-  usage
-  exit
+  umount $MNT_DIR
 fi
 
-if [ ! -d rootfs ]
-then
-  echo "$ERROR could not find 'rootfs' directory in '$PWD'."
-  usage
-  exit
-fi
+message "mounting boot partition"
 
-if [ ! -d mnt ]
-then
-  mkdir mnt
-fi
+mount -t vfat $LOOP_IMG_BOOT $MNT_DIR
 
-BLKSIZE=512
+message "copying firware, linux kernel and its configuration"
+cp -r $FIRWARE_DIR/* $MNT_DIR
+cat $CONFIG_DIR/cmdline.txt | tee $MNT_DIR/cmdline.txt > $REDIRECT 2>&1
 
-du rootfs -s -B $BLKSIZE > size.tmp
-read SIZE DIR < size.tmp
-rm size.tmp
-
-echo "=> creating empty image of size $SIZE x $BLKSIZE..."
-dd if=/dev/zero of=$1 bs=$BLKSIZE count=$SIZE
-
-echo "=> installing ext3 filesystem..."
-yes | sudo mke2fs -t ext3 $1
-
-echo "=> mounting new empty image."
-./mount.sh $1
-
-echo "=> copying and backing up root file system content..."
-./backup.sh > /dev/null
-sudo cp -pr rootfs/* mnt
 sync
+umount $MNT_DIR
 
-echo "=> unmounting image."
-sudo umount mnt
+message "mounting root partition"
+mount -t ext2 $LOOP_IMG_RFS $MNT_DIR
+message "populating file system"
+cp -r $RFS_DIR/* $MNT_DIR
+sync
+umount $MNT_DIR
 
-echo "done."
+######################################################################################################################################################
+# final clearup
+######################################################################################################################################################
+
+message "releasing loop devices '$LOOP'"
+
+losetup -d $LOOP
