7af62c595c80d5aa361496272a605ae6e0affd13
Pierre <pierre_langlois@etu.u-bourgogne.fr>
2012-12-02 02:46:13 +0100

    same coloration as mkimage.sh

--- setup.sh
+++ setup.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
-echo "checking crosstool-ng..."
 
-ct-ng version
+echo -e "\033[1m\033[32m====> \033[34mchecking crosstool-ng...\033[0m"
 
-if [ ! $? ]
+if [ ! `which ct-ng 2> /dev/null; echo #?` ]
 then
   echo "you need to have crosstool-ng installed on your system."
+  usage
   exit
 fi
 
-echo "setting up project..."
+echo -e "\033[1m\033[32m====> \033[34msetting up project...\033[0m"
 
 mkdir -p build rootfs tarballs toolchains images mnt archives
